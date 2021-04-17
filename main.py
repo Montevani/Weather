a0494706ce2ab4b38af6798235c6eb93cbd77619
José Miguel <76499117+Montevani@users.noreply.github.com>
2021-04-17 14:06:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 from pprint import pprint as pp
-API_Key = '157b2d0bef11e1fafc79c4aadf1858a4'
-city = input ("Enter your city: ")
-base_url = "http://api.openweathermap.org/data/2.5/weather?appid=" +API_Key+"&q="+city
-weather_data = requests.get(base_url).json()
+API_Key = '157b2d0bef11e1fafc79c4aadf1858a4' #I sincerely don't care about this API key, feel free to steal it
+city = input ("Please enter your city's name: ")
+url = "http://api.openweathermap.org/data/2.5/weather?appid=" +API_Key+"&q="+city
+weather_data = requests.get(url).json()
 pp(weather_data)
